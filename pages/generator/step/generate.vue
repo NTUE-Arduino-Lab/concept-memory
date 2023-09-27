c612c2cd49ba73b0bc6ef48c7ad62607aad812e5
Peiyun Lee <may1092200258@gmail.com>
2023-09-27 11:58:19 +0800

    update request url

--- pages/generator/step/generate.vue
+++ pages/generator/step/generate.vue
@@ -12,7 +12,10 @@ const requestData = useState('requestData', () => postersStore.sdRequestData)
 const loading = useState('loading', () => true)
 const canvas = ref<HTMLCanvasElement | null>(null)
 
-const { pending: pending, data: poster, error: error } = useFetch('https://aiposter.iiiedu.net/sdapi/sdapi/v1/txt2img', {
+// const url = "https://aiposter.iiiedu.net/sdapi/sdapi/v1/txt2img"
+const url = "http://44.218.216.41:7860/sdapi/v1/txt2img"
+
+const { pending: pending, data: poster, error: error } = useFetch(url, {
     method: 'post',
     lazy: true,
     body: requestData,
